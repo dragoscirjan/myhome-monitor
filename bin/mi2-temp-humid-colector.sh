b2d203b15d163e63923c870337507aa9bbff6224
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2022-03-14 23:10:55 +0200

    preparing colector 3

--- bin/mi2-temp-humid-colector.sh
+++ bin/mi2-temp-humid-colector.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
-gateway_url=http://192.168.1.3:9091
-
 # /home/ubuntu/myhome-monitor/3rdparty/MiTemperature2/../../bin/mi2-temp-humid-to-file.sh sensorname,temperature,humidity,voltage,batteryLevel,timestamp kidsroom 23.77 46 3.094 99 1647290484
 
 cat <<EOF >> /tmp/mi2-temp-humid-colector.gauge
-# HELP test_temp_celsius Temperature, celsius
 # TYPE test_temp_celsius gauge
+# HELP test_temp_celsius Temperature, celsius
 test_temp_celsius{sensor="$2"} $3
-# HELP test_humidity_pct Humidity, percentage
 # TYPE test_humidity_pct gauge
+# HELP test_humidity_pct Humidity, percentage
 test_humidity_pct{sensor="$2"} $4
-# HELP test_battery_voltage Battery, voltage
 # TYPE test_battery_voltage gauge
+# HELP test_battery_voltage Battery, voltage
 test_battery_voltage{sensor="$2"} $5
-# HELP test_battery_pct Humidity, percentage
 # TYPE test_battery_pct gauge
+# HELP test_battery_pct Humidity, percentage
 test_battery_pct{sensor="$2"} $6
 EOF
--- bin/mi3-temp-humit-to-pushgateway.sh
+++ bin/mi3-temp-humit-to-pushgateway.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
-curl -X POST -H "Content-type: text/plain" --data "$(cat /tmp/mi2-temp-humid-colector.gauge)" $gateway_url/metrics/job/tempBatch/instance
+gateway_url=http://192.168.1.3:9091
+
+cat /tmp/mi2-temp-humid-colector.gauge \
+| curl -X POST -H "Content-type: text/plain" \
+--data-binary @- $gateway_url/metrics/job/tempBatch
+
 echo "" > /tmp/mi2-temp-humid-colector.gauge
