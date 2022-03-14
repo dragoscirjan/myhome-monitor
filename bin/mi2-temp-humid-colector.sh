a9e2ceebae0308c44c4d31accdda6a49614c825d
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2022-03-14 23:16:03 +0200

    preparing colector 4

--- bin/mi2-temp-humid-colector.sh
+++ bin/mi2-temp-humid-colector.sh
@@ -2,7 +2,16 @@
 
 # /home/ubuntu/myhome-monitor/3rdparty/MiTemperature2/../../bin/mi2-temp-humid-to-file.sh sensorname,temperature,humidity,voltage,batteryLevel,timestamp kidsroom 23.77 46 3.094 99 1647290484
 
-cat <<EOF >> /tmp/mi2-temp-humid-colector.gauge
+push_file = /tmp/mi2-temp-humid-colector.push
+add_types = 1
+
+if [ -f $push_file ]; then
+    grep TYPE $push_file -RIn && add_types=0
+fi
+
+if [ $add_types -eq 1 ]; then
+    
+    cat <<EOF >> $push_file
 # TYPE test_temp_celsius gauge
 # HELP test_temp_celsius Temperature, celsius
 test_temp_celsius{sensor="$2"} $3
@@ -16,3 +25,14 @@ test_battery_voltage{sensor="$2"} $5
 # HELP test_battery_pct Humidity, percentage
 test_battery_pct{sensor="$2"} $6
 EOF
+    
+else
+    
+    cat <<EOF >> $push_file
+test_temp_celsius{sensor="$2"} $3
+test_humidity_pct{sensor="$2"} $4
+test_battery_voltage{sensor="$2"} $5
+test_battery_pct{sensor="$2"} $6
+EOF
+    
+fi
