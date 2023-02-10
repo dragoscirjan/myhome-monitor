65070ac49561c3382a403d4790217a2735bac028
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2023-02-11 00:51:22 +0200

    +

--- os/pi4/init.sh
+++ os/pi4/init.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
-base_dir=$(dirname $0)
+# base_dir=$(dirname $0)
 
-sudo bash $(dirname $0)/../init.sh
+# sudo bash $(dirname $0)/../init.sh
 
-sudo rm -rf /opt/grafana-setup
-sudo git clone https://github.com/dragoscirjan/grafana-setup.git /opt/grafana-setup
+# sudo rm -rf /opt/grafana-setup
+# sudo git clone https://github.com/dragoscirjan/grafana-setup.git /opt/grafana-setup
 
-cd /opt/grafana-setup && sudo make up
+# cd /opt/grafana-setup && sudo make up
 
-cd $base_dir
+# cd $base_dir
+
+
+crontab -r
+cat <<EOF
+*/30 * * * *    ubuntu  bash $(dirname $0/../..)/bin/crontab-mi2-temp-humid-sensors.sh east
+EOF
