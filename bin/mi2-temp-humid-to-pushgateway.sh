49878ac322af29e4ff612d10ab0be706eab26714
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2023-02-11 00:42:03 +0200

    +

--- bin/mi2-temp-humid-to-pushgateway.sh
+++ bin/mi2-temp-humid-to-pushgateway.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-source $(dirname $0).env
+source $(dirname $0)/.env
 
-if [ -f .env.local ]; then
-    source .env.local
+if [ -f $(dirname $0)/.env.local ]; then
+    source $(dirname $0)/.env.local
 fi
 
 push_file=/tmp/mi2-temp-humid-colector.prom
