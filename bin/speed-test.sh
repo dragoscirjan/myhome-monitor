1bb7c2866d12869b2fdcb40a68a9f7968971848a
Dragos Cirjan <dragos.cirjan@cellebrite.com>
2023-02-12 22:20:22 +0200

    +

--- bin/speed-test.sh
+++ bin/speed-test.sh
@@ -10,15 +10,18 @@ speed_data=$(speedtest --json)
 
 if_name=$(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//')
 
+if_dld_speed=$(echo $speed_data | jq ".download" -r)
+if_dld_speed=$(echo "scale=2 ; $if_dld_speed / 1" | bc)
+if_uld_speed=$(echo $speed_data | jq ".upload" -r)
+if_uld_speed=$(echo "scale=2 ; $if_uld_speed / 1" | bc)
+if_ping=$(echo $speed_data | jq ".ping" -r)
+if_ping=$(echo "scale=2 ; $if_ping / 1" | bc)
+
 if_ssid_name=$(nmcli -t -f name,device connection show --active | grep $if_name | cut -d\: -f1)
 
 if [ "$if_ssid_name" == "" ]; then
   if_ssid_name=$(iw dev $if_name info | grep ssid | awk '{print $2}')
 fi
-
-# if_rx_bitrate=$(cat /proc/net/dev | grep $if_name | awk '{print $2}')
-# if_tx_bitrate=$(cat /proc/net/dev | grep $if_name | awk '{print $10}')
-
 
 push_file=/tmp/speed-test.prom
 add_types=1
@@ -34,13 +37,13 @@ if [ $add_types -eq 1 ]; then
   cat <<EOF >> $push_file
 # TYPE ${prefix}net_download gauge
 # HELP ${prefix}net_download Download Speed, MBps
-${prefix}net_download{if="$if_name"}{ssid="$if_ssid_name"} $(echo $speed_data | jq ".download" -r)
+${prefix}net_download{if="$if_name"}{ssid="$if_ssid_name"} $if_dld_speed
 # TYPE ${prefix}net_download gauge
 # HELP ${prefix}net_download Download Speed, MBps
-${prefix}net_upload{if="$if_name"}{ssid="$if_ssid_name"} $(echo $speed_data | jq ".upload" -r)
+${prefix}net_upload{if="$if_name"}{ssid="$if_ssid_name"} $if_uld_speed
 # TYPE ${prefix}net_ping gauge
 # HELP ${prefix}net_ping Download Speed, seconds
-${prefix}net_upload{if="$if_name"}{ssid="$if_ssid_name"} $(echo $speed_data | jq ".ping" -r)
+${prefix}net_upload{if="$if_name"}{ssid="$if_ssid_name"} $if_ping
 EOF
 
 else
